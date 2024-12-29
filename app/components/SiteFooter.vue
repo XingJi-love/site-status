1e1987239eb6cdf547844a23bbf491d9ba60d63a
XingJi-love <3968523679@qq.com>
2024-12-29 20:43:21 +0800

    Update SiteFooter.vue

--- app/components/SiteFooter.vue
+++ app/components/SiteFooter.vue
@@ -50,9 +50,9 @@ const { public: configPublic } = useRuntimeConfig();
 const { siteIcp, version } = configPublic;
 
 const linkData = {
-  github: "https://github.com/imsyy/site-status",
-  home: "https://www.imsyy.top",
-  email: "mailto:one@imsyy.top",
+  github: "https://github.com/XingJi-love/site-status",
+  home: "https://home.xingji.fun",
+  email: "3326502459@qq.com",
 };
 </script>
 
